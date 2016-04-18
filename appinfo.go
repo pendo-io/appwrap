b5b845c955981d19c79c20f85e1b8ed825f0aecd
Erik Troan <ewt@troan.org>
2016-04-18 14:18:22 -0400

    added AppengineInfo{}

---
+++ appinfo.go
@@ -0,0 +1,36 @@
+// +build appengine appenginevm
+
+package appwrap
+
+import (
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/module"
+)
+
+type AppengineInfo interface {
+	ModuleName() string
+	VersionID() string
+	ModuleDefaultVersionID(moduleName string) (string,error)
+}
+
+type AppengineInfoFromContext struct {
+	c context.Context
+}
+
+func NewAppengineInfoFromContext(c context.Context) AppengineInfo{
+	return AppengineInfoFromContext{c}
+}
+
+func (ai AppengineInfoFromContext) ModuleName() string {
+	return appengine.ModuleName(ai.c)
+}
+
+func (ai AppengineInfoFromContext) VersionID() string {
+	return appengine.VersionID(ai.c)
+}
+
+func (ai AppengineInfoFromContext) ModuleDefaultVersionID(moduleName string) (string, error) {
+	return module.DefaultVersion(ai.c, moduleName)
+}
+
