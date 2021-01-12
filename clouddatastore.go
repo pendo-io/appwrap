ed435fc9d186853714c65fee576e9ef048807f08
Jean-Claude Shore <jshore@pendo.io>
2021-01-12 09:20:50 -0500

    Disable retries internal to the datastore library
    The calling code already has a backoff spec implemented, and so this is
    redundant

--- clouddatastore.go
+++ clouddatastore.go
@@ -276,7 +276,7 @@ func (cds CloudDatastore) RunInTransaction(f func(coreds DatastoreTransaction) e
 				transaction: transaction,
 			}
 			return f(ct)
-		})
+		}, datastore.MaxAttempts(1))
 		return err
 	})
 
