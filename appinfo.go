4370eff4d9f7f5abdaddcf23915b90bace3eb59c
Jeff Bornemann <jeff.bornemann@pendo.io>
2021-02-04 17:55:32 -0500

    APP-31735 kubernetes implmentation of appinfo

--- appinfo.go
+++ appinfo.go
@@ -1,12 +1,22 @@
 package appwrap
 
-import "google.golang.org/appengine"
+import (
+	"context"
+	"fmt"
+	"os"
+	"sync"
+
+	"cloud.google.com/go/compute/metadata"
+	"google.golang.org/appengine"
+	istio "istio.io/client-go/pkg/clientset/versioned"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
 
 type AppengineInfo interface {
 	DataProjectID() string
 	NativeProjectID() string
 	InstanceID() string
-	ModuleDefaultVersionID(moduleName string) (string, error)
 	ModuleHasTraffic(moduleName, moduleVersion string) (bool, error)
 	// ModuleHostname returns the HTTP hostname to route to the given version, module, and app (project).
 	// If version is "", the *live* version is used.
@@ -21,8 +31,54 @@ type AppengineInfo interface {
 }
 
 var (
+	zone    string
+	zoneMtx sync.Mutex
+)
+
+func Zone() string {
+	zoneMtx.Lock()
+	defer zoneMtx.Unlock()
+
+	if zone == "" {
+		z, err := metadata.Zone()
+		if err != nil {
+			panic(err)
+		}
+		zone = z
+	}
+
+	return zone
+}
+
+var (
 	IsDevAppServer = false
 	IsFlex         = appengine.IsFlex
 	IsSecondGen    = appengine.IsSecondGen
 	IsStandard     = appengine.IsStandard
 )
+
+// Don't call this.  It exists to make NewAppengineInfoFromContext mockable
+func InternalNewAppengineInfoFromContext(c context.Context) AppengineInfo {
+	// if running in K8s, the following environment variable will always be set
+	if os.Getenv("KUBERNETES_SERVICE_HOST") != "" {
+		config, err := rest.InClusterConfig()
+		if err != nil {
+			panic(fmt.Sprintf("Cannot get K8s config: %s", err.Error()))
+		}
+		clientset, err := kubernetes.NewForConfig(config)
+		if err != nil {
+			panic(fmt.Sprintf("Cannot create K8s client: %s", err.Error()))
+		}
+		istioset, err := istio.NewForConfig(config)
+		if err != nil {
+			panic(fmt.Sprintf("Cannot create Istio k8s client: %s", err.Error()))
+		}
+		return AppengineInfoK8s{
+			c:         c,
+			clientset: clientset,
+			istioset:  istioset,
+		}
+	}
+
+	return AppengineInfoFlex{c: c}
+}
---
+++ appinfok8s.go
@@ -0,0 +1,117 @@
+package appwrap
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	istio "istio.io/client-go/pkg/clientset/versioned"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/kubernetes"
+)
+
+type AppengineInfoK8s struct {
+	c         context.Context
+	clientset kubernetes.Interface
+	istioset  istio.Interface
+}
+
+func (ai AppengineInfoK8s) DataProjectID() string {
+	if project := os.Getenv("GOOGLE_CLOUD_DATA_PROJECT"); project != "" {
+		return project
+	}
+
+	return os.Getenv("GOOGLE_CLOUD_PROJECT")
+}
+
+func (ai AppengineInfoK8s) NativeProjectID() string {
+	return os.Getenv("GOOGLE_CLOUD_PROJECT")
+}
+
+func (ai AppengineInfoK8s) InstanceID() string {
+	return os.Getenv("K8S_POD")
+}
+
+// ModuleHostname in K8s doesn't yet handle versions
+func (ai AppengineInfoK8s) ModuleHostname(version, module, app string) (string, error) {
+	if module == "" {
+		module = ai.ModuleName()
+	}
+	if app == "" {
+		app = ai.NativeProjectID()
+	}
+
+	domain := os.Getenv("K8S_DOMAIN")
+
+	return fmt.Sprintf("%s-dot-%s.%s", module, app, domain), nil
+}
+
+func (ai AppengineInfoK8s) ModuleName() string {
+	return os.Getenv("K8S_SERVICE")
+}
+
+func (ai AppengineInfoK8s) VersionID() string {
+	return os.Getenv("K8S_VERSION")
+}
+
+func (ai AppengineInfoK8s) Zone() string {
+	//This uses GCE metadata service, which is available on the nodes of this pod
+	return Zone()
+}
+
+//There is no one way to achieve traffic management in k8s. This implementation assumes using Istio.
+//
+//This implementation will attempt to find a corresponding virtual service resource labeled app=<module_name>.
+//We will attempt to find a destination rule will a subset named moduleVersion, and derive traffic weight from this
+func (ai AppengineInfoK8s) ModuleHasTraffic(moduleName, moduleVersion string) (bool, error) {
+	namespace := ai.NativeProjectID()
+	labelSet := labels.Set{
+		"app": moduleName,
+	}
+	if vsl, err := ai.istioset.NetworkingV1beta1().VirtualServices(namespace).List(ai.c, metav1.ListOptions{
+		LabelSelector: labelSet.String(),
+	}); err != nil {
+		return false, fmt.Errorf("could not get virtual service for %s in namespace %s, error was: %s", moduleName, namespace, err.Error())
+	} else {
+		for _, svc := range vsl.Items {
+			httpRoutes := svc.Spec.Http
+			tlsRoutes := svc.Spec.Tls
+			//Find a tls or http route. Although same named attributes, different structures, so we loop through independently
+			for _, route := range tlsRoutes {
+				for _, tlsDest := range route.Route {
+					if tlsDest.Destination.Subset == moduleVersion && tlsDest.Weight > 0 {
+						return true, nil
+					}
+				}
+			}
+			for _, route := range httpRoutes {
+				for _, httpDest := range route.Route {
+					if httpDest.Destination.Subset == moduleVersion && httpDest.Weight > 0 {
+						return true, nil
+					}
+				}
+			}
+		}
+	}
+	return false, nil
+}
+
+func (ai AppengineInfoK8s) NumInstances(moduleName, version string) (int, error) {
+	namespace := ai.NativeProjectID()
+	labelSet := labels.Set{
+		"version": version,
+		"app":     moduleName,
+	}
+	if rs, err := ai.clientset.AppsV1().ReplicaSets(namespace).List(ai.c, metav1.ListOptions{
+		LabelSelector: labelSet.String(),
+	}); err != nil {
+		return -1, fmt.Errorf("could not get number of instances for module %s version %s in namespace %s, error was: %s", moduleName, version, namespace, err.Error())
+	} else {
+		instances := 0
+		for _, set := range rs.Items {
+			instances = +int(set.Status.ReadyReplicas)
+		}
+		return instances, nil
+	}
+}
